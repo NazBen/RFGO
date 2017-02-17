7ed502fb19623042d38c82f339c96725d1e426f8
NazBen <nazih.benoumechiara@gmail.com>
2017-02-17 16:25:17 +0100

    First commit: adding the starting files

---
+++ rfgo/rfgo.py
@@ -0,0 +1,15 @@
+"""Random Forest Global Optimization.
+The main class iteratively determine the minimum and maximum of a stochastic
+model.
+"""
+
+
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+
+class RFGO(object):
+    """
+    """
+    def __init__(self):
+        pass
